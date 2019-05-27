869ea1c7ddfa07bb2b9b366630c3634c6b5844ce
FoxyCat1600 <yuuto-f-17e@stu.kbc.ac.jp>
2019-05-27 09:59:21 +0900

    録画用アセットとゲームオーバーを手直し、ゲームクリアもムービーを再生して終わるように変更しました。

---
+++ TGS2019_TeamD/Assets/GameOverScript/Blink.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blink : MonoBehaviour {
+   [SerializeField] float alpha = 1f;
+    float Speed = 0.015f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.GetComponent<CanvasRenderer>().SetAlpha(alpha);
+        alpha = alpha - Speed;
+
+        if(alpha <= 0 || alpha >= 1)
+        {
+            Speed = Speed * -1;
+        }
+    
+    }
+}
